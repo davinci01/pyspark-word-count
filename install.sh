4e53f0556a207cd29fc86d75093e23f360680c91
rbih boulanouar <32702919+davinci01@users.noreply.github.com>
2021-11-23 21:40:31 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+# Run below commands in google colab
+# install Java8
+!apt-get install openjdk-8-jdk-headless -qq > /dev/null
+# download spark3.0.0
+!wget -q http://apache.osuosl.org/spark/spark-3.1.2/spark-3.1.2-bin-hadoop3.2.tgz
+# unzip it
+!tar xf spark-3.1.2-bin-hadoop3.2.tgz
+# install findspark 
+!pip install -q findspark
